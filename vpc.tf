5058678f5f369eec49996fb03996d9d0a1ffa0c6
Koshy <koshy.muthalaly88@gmail.com>
2021-05-29 07:27:42 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,34 +1,13 @@
-module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 3.0"
-
-    project_id   = "var.project"
-    network_name = "kapella-vpc"
-    routing_mode = "GLOBAL"
-
-    subnets = [
-        {
-            subnet_name           = "kapella-subnet"
-            subnet_ip             = "10.100.10.0/24"
-            subnet_region         = "var.region"
-        }
-    ]
-
-    routes = [
-        {
-            name                   = "egress-internet"
-            description            = "route through IGW to access internet"
-            destination_range      = "0.0.0.0/0"
-            tags                   = "egress-inet"
-            next_hop_internet      = "true"
-        },
-        {
-            name                   = "app-proxy"
-            description            = "route through proxy to reach app"
-            destination_range      = "10.50.10.0/24"
-            tags                   = "app-proxy"
-            next_hop_instance      = "app-proxy-instance"
-            next_hop_instance_zone = "us-west1-a"
-        },
-    ]
+module "network" {
+  source  = "app.terraform.io/Koshy-training/network/google"
+  version = "3.2.2"
+  network_name ="kapella-network"
+  project_id = var.project
+  subnets = [
+  {
+    subnet_name   = "kapella-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
 }
